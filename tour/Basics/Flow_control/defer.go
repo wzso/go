ea2bf709e2655e80b053038b133f14cf136e4369
Vincent <wenzhishen@icloud.com>
2017-06-08 11:11:43 +0800

    review defer

--- tour/Basics/Flow_control/defer.go
+++ tour/Basics/Flow_control/defer.go
@@ -3,23 +3,28 @@ package main
 import "fmt"
 
 func foo() string {
-	fmt.Printf("\\\\\\\\\\\n")
-	return "bluez"
+	fmt.Println("AAA.")
+	return "foo"
+}
+
+func foo2() string {
+	defer fmt.Println("BBB.")
+	return "foo2"
 }
 
 // defer stack, LIFO
 // https://blog.go-zh.org/defer-panic-and-recover
 func multiDefer() {
 	for i := 0; i < 6; i++ {
-		defer fmt.Println("for-loop", i)
+		defer fmt.Println("CCC.", i)
 	}
 }
 
 func main() {
 	// 提供参数的 foo 会即刻，而defer后面的语句延迟到函数即将返回时才调用。
-	defer fmt.Println("I am in a defer scope.", foo())
+	defer fmt.Println("DDD.", foo(), foo2())
 
 	multiDefer()
 
-	fmt.Println("Function is going to end.")
+	fmt.Println("EEE.")
 }
